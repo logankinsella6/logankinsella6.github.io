2ddd9689d859b5369d26f82891d718e7386281dc
Logan Kinsella <logankinsella2@gmail.com>
2021-01-11 17:49:08 -0600

    PogU

--- request.py
+++ request.py
@@ -1,13 +1,21 @@
 import requests
+import webbrowser
 
-url = 'https://classroom.powerschool.com/'
+url = 'google.com'
+pDownY = 'Yes.'
+pDown = 'Powerschool is down.'
+pDownN = 'No.'
+pDownZ = 'Powerschool is running'
 
 request_response = requests.head(url)
 status_code = request_response.status_code
 website_is_up = status_code == 200
 
 if website_is_up == False:
-    print('No')
-
+    html_content = f"<html> <head> </head> {pDownY} <body> <p> {pDown} </p> </body> </html>"
+    with open("index.html", "w") as html_file:
+        html_file.write(html_content)
 if website_is_up == True:
-    print('Yes')
+    html_content = f"<html> <head> </head> {pDownN} <body> <p> {pDownZ} </p> </body> </html>"
+    with open("index.html", "w") as html_file:
+        html_file.write(html_content)
