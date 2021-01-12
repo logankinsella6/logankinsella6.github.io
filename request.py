083c4f3c303f01ca8ead06e64eeccfffb92c467c
Logan Kinsella <logankinsella2@gmail.com>
2021-01-11 18:44:56 -0600

    PLEASE WORK

--- request.py
+++ request.py
@@ -2,20 +2,17 @@ import requests
 import webbrowser
 
 url = 'google.com'
-pDownY = 'Yes.'
-pDown = 'Powerschool is down.'
-pDownN = 'No.'
-pDownZ = 'Powerschool is running'
 
 request_response = requests.head(url)
 status_code = request_response.status_code
 website_is_up = status_code == 200
 
 if website_is_up == False:
-    html_content = f"<html> <head> </head> {pDownY} <body> <h1> </h1><p> {pDown} </p> </body> </html>"
-    with open("index.html", "w") as html_file:
-        html_file.write(html_content)
+    html_content = "<html> <head> </head> <h2> Yes. </h2> <body> <p> Powerschool is down! </p> </body> </html>"
+
 if website_is_up == True:
-    html_content = f"<html> <head> </head> {pDownN} <body> <p> {pDownZ} </p> </body> </html>"
-    with open("index.html", "w") as html_file:
-        html_file.write(html_content)
+    html_content = "<html> <head> </head> <h2> No. </h2> <body> <p> Poweschool is up! </p> </body> </html>"
+
+outfile = ("index.html", "w")
+outfile.write(html_content)
+outfile.close()
