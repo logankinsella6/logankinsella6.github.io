0e30578cb3d9f29ca5526802e28d462bb16f8e78
Logan Kinsella <logankinsella2@gmail.com>
2021-01-11 18:21:00 -0600

    PogU

--- request.py
+++ request.py
@@ -12,7 +12,7 @@ status_code = request_response.status_code
 website_is_up = status_code == 200
 
 if website_is_up == False:
-    html_content = f"<html> <head> </head> {pDownY} <body> <p> {pDown} </p> </body> </html>"
+    html_content = f"<html> <head> </head> {pDownY} <body> <h1> </h1><p> {pDown} </p> </body> </html>"
     with open("index.html", "w") as html_file:
         html_file.write(html_content)
 if website_is_up == True:
